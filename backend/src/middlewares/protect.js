1f6e62a942e37b287942e00ef06ec08c45823ecd
Alisha Aggarwal <aggarwalalisha071@gmail.com>
2025-05-27 18:05:42 +0530

    made creating post API

---
+++ backend/src/middlewares/protect.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import { apiResponse } from "../utils/common.js";
+
+const protect = async (req, res, next) => {
+  try {
+    const { blog_application } = req.cookies;
+
+    if (!blog_application) {
+      return apiResponse(res, 401, "error", "User not authorized!");
+    }
+
+    const decodedToken = jwt.verify(socialbuzzy, process.env.JWT_SECRET);
+    req.user = decodedToken;
+    next();
+  } catch (err) {
+    console.log("Error in authorizing user: ", err.message);
+    apiResponse(res, 500, "error", "Error in authorizing user!", err.message);
+  }
+};
+
+export default protect;
